f33d9131210e1cebc35bd9c034b947a14c2ef45c
ipetrovski3 <ipetrovski3@gmail.com>
2021-02-13 21:47:17 +0100

    gem pagy added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
+    @pagy, @products = pagy(@category.products.all, items: 6)
   end
 end

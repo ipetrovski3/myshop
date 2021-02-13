bfcb40c0885805dcf7f7a25f6ef4791c32c49181
ipetrovski3 <ipetrovski3@gmail.com>
2021-02-13 16:01:01 +0100

    added price tables in orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,19 @@ class Order < ApplicationRecord
 
   belongs_to :product
 
+  before_save :set_price
+  before_save :set_total_price
+
   scope :order_shipped, -> { where(completed: true) }
   scope :unproccesed_order, -> { where(completed: false) }
+
+  private
+
+  def set_total_price
+    self.total_price = qty * set_price
+  end
+
+  def set_price
+    self.price = product.price
+  end
 end
